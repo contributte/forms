dde59bd4f787f22af02c58f43b901d5b71d4fe72
cranycrane <jakub.jerabek@outlook.com>
2025-02-06 17:05:56 +0100

    Feature: Bootstrap5 support

---
+++ src/Rendering/Bootstrap5InlineRenderer.php
@@ -0,0 +1,103 @@
+<?php declare(strict_types = 1);
+
+namespace Contributte\Forms\Rendering;
+
+use Nette\Forms\Controls;
+use Nette\Forms\Form;
+use Nette\Forms\Rendering\DefaultFormRenderer;
+
+class Bootstrap5InlineRenderer extends DefaultFormRenderer
+{
+
+  /** @var mixed[] */
+	public $wrappers = [
+	'form' => [
+	  'container' => '',
+	],
+	'error' => [
+	  'container' => 'div class="alert alert-danger"',
+	  'item' => 'p',
+	],
+	'group' => [
+	  'container' => 'fieldset',
+	  'label' => 'legend',
+	  'description' => 'p',
+	],
+	'controls' => [
+	  'container' => '',
+	],
+	'pair' => [
+	  'container' => 'div class="mb-3"',
+	  '.required' => 'required',
+	  '.optional' => null,
+	  '.odd' => null,
+	],
+	'control' => [
+	  'container' => null,
+	  '.odd' => null,
+	  'description' => 'span class="form-text"',
+	  'requiredsuffix' => '',
+	  'errorcontainer' => 'span class="form-text"',
+	  'erroritem' => '',
+	  '.required' => 'required',
+	  '.text' => 'text',
+	  '.password' => 'text',
+	  '.file' => 'text',
+	  '.submit' => 'button',
+	  '.image' => 'imagebutton',
+	  '.button' => 'button',
+	],
+	'label' => [
+	  'container' => '',
+	  'suffix' => null,
+	  'requiredsuffix' => '',
+	],
+	'hidden' => [
+	  'container' => 'div',
+	],
+	];
+
+  /**
+   * Provides complete form rendering.
+   *
+   * @param string|null $mode 'begin', 'errors', 'ownerrors', 'body', 'end' or empty to render all
+   */
+	public function render(Form $form, $mode = null): string
+	{
+		$form->getElementPrototype()->addClass('d-inline-flex gap-2'); // Bootstrap 5 nahrazení form-inline
+
+		$onlyButton = Helpers::onlyOneButton($form);
+
+		foreach ($form->getControls() as $control) {
+			if ($control instanceof Controls\BaseControl) {
+				$control->getLabelPrototype()->addClass('col-form-label');
+			}
+
+			switch (true) {
+				case $control instanceof Controls\Button:
+					if (!Helpers::htmlClassContains($control->getControlPrototype(), 'btn')) {
+						$control->getControlPrototype()->addClass($onlyButton ? 'btn btn-primary' : 'btn btn-secondary');
+					}
+
+					break;
+
+				case $control instanceof Controls\TextBase:
+				case $control instanceof Controls\SelectBox:
+				case $control instanceof Controls\MultiSelectBox:
+					$control->getControlPrototype()->addClass('form-control me-3'); // me-3 místo mx-sm-3
+					break;
+
+				case $control instanceof Controls\Checkbox:
+				case $control instanceof Controls\CheckboxList:
+				case $control instanceof Controls\RadioList:
+					$control->getSeparatorPrototype()->setName('div')->addClass('form-check form-check-inline');
+					$control->getControlPrototype()->addClass('form-check-input');
+					$control->getLabelPrototype()->addClass('form-check-label');
+					break;
+			}
+		}
+
+		return parent::render($form, $mode);
+	}
+
+}
